c73f6161f6c5689db722a092bed3a85f733b2255
calihuddleston <calihuddleston@gmail.com>
2022-10-03 23:06:32 -0400

    adding update/delete functionality to user controller, finishing up api routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,4 +22,33 @@ module.exports = {
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.status(500).json(err));
   },
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with this ID" })
+          : res.json(user)
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  deleteUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with this ID" })
+          : User.findOneAndUpdate(
+              { thoughts: req.params.thoughtId },
+              { $pullAll: { thoughts: req.params.thoughtId } },
+              { new: true }
+            )
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
