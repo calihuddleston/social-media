4b4815deecef1937f5c3317ab57e96b1a5a63766
calihuddleston <calihuddleston@gmail.com>
2022-10-15 12:01:51 -0400

    updated user controller to have add friend and delete friend

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,8 @@
 const { connect, connection } = require("mongoose");
+
+connect("mongodb://127.0.0.1:27017/userandthoughtsDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = connection;
--- controllers/userController.js
+++ controllers/userController.js
@@ -51,4 +51,30 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.params.friendId } },
+      { new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with this ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  deleteFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendId } },
+      { new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with this ID" })
+          : res.json({ message: "Friend successfully deleted!" })
+      )
+      .catch((err) => res.status(505).json(err));
+  },
 };
